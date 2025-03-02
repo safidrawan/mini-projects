dd7bfc92e4d9f3f97579b4921e8e83043c7ef7e0
Mohammad Rafi Safidrawan <safidrawan@gmail.com>
2025-03-02 11:06:25 +0430

    Nav links added and dynamic user

--- src/main.jsx
+++ src/main.jsx
@@ -10,15 +10,22 @@ import {
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "./Layout.jsx";
-import Home from "./components/Home.jsx"
-import About from "./components/About.jsx"
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import User from "./components/User.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<Layout />}>
-    <Route path="" element={<Home />} />
-    <Route path="about" element={<About />} />
-  </Route>)
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/" element={<User />}>
+        <Route path=":userid" element={<User />} />
+      </Route>
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
