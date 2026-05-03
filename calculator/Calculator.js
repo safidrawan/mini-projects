0e69e653ad2560b24b52c3a3000f8d02fa41798b
safidrawan <safidrawan@gmail.com>
2026-05-03 18:12:45 +0430

    formatting added

--- calculator/Calculator.js
+++ calculator/Calculator.js
@@ -1,9 +1,16 @@
 
 export default class Calculator {
+
     constructor(primary, secondary, operation) {
+        const FORMATTER = new Intl.NumberFormat('en', {
+            maximumFractionDigits: 10,
+        });
+        this.formatter = FORMATTER;
         this.primaryDisplay = primary;
         this.secondaryDisplay = secondary;
         this.operationDisplay = operation;
+        this.primaryDisplay.data = { value: 0 };
+        this.secondaryDisplay.data = { value: 0 };
         this.reset = false;
         this.clearAll();
     }
@@ -11,38 +18,61 @@ export default class Calculator {
     clearAll() {
         this.primaryDisplay.textContent = 0;
         this.secondaryDisplay.textContent = '';
+        this.primaryDisplay.data.value = '0';
+        this.secondaryDisplay.data.value = '0';
         this.operationDisplay.textContent = '';
     }
 
     delete() {
-        let data = this.primaryDisplay.textContent.split('');
-        this.primaryDisplay.textContent = data;
-
+        let data = String(this.primaryDisplay.data.value).slice(0, -1);
+        this.primaryDisplay.data.value = data;
+        this.primaryDisplay.textContent = this.formatter.format(data);
+        if (this.operationDisplay.textContent === '') {
+            this.secondaryDisplay.textContent = this.primaryDisplay.textContent;
+            this.secondaryDisplay.data.value = this.primaryDisplay.data.value;
+        }
     }
 
     addDigit(digit) {
-        if (this.primaryDisplay.textContent === '0' || this.reset) {
+        if (this.primaryDisplay.data.value === '0' || this.reset) {
             this.primaryDisplay.textContent = '';
+            this.primaryDisplay.data.value = '';
             this.reset = false;
         }
-        this.primaryDisplay.textContent += digit;
+        if (this.primaryDisplay.textContent.includes('.') && digit === '.') return;
+        this.primaryDisplay.data.value += digit;
+
+        this.primaryDisplay.textContent = this.formatter.format(this.primaryDisplay.data.value);
 
         if (this.operationDisplay.textContent === '') {
             this.secondaryDisplay.textContent = this.primaryDisplay.textContent;
         }
 
-    } 
+    }
 
     operation(oeperator) {
+        if (this.operationDisplay.textContent !== '' && this.primaryDisplay.data.value !== '') {
+            this.equals();
+        }
+        if (this.primaryDisplay.data.value === '') {
+            this.operationDisplay.textContent = oeperator;
+            return;
+        }
+
         this.operationDisplay.textContent = oeperator;
+
         this.secondaryDisplay.textContent = this.primaryDisplay.textContent;
+        this.secondaryDisplay.data.value = this.primaryDisplay.data.value;
+
         this.primaryDisplay.textContent = '';
+        this.primaryDisplay.data.value = '';
 
     }
 
     equals() {
-        const a = parseFloat(this.secondaryDisplay.textContent);
-        const b = parseFloat(this.primaryDisplay.textContent);
+        if (this.operationDisplay.textContent === '' || this.primaryDisplay.data.value === '') return;
+        const a = parseFloat(this.secondaryDisplay.data.value);
+        const b = parseFloat(this.primaryDisplay.data.value);
         const operator = this.operationDisplay.textContent;
         let result;
         switch (operator) {
@@ -59,8 +89,10 @@ export default class Calculator {
                 result = a / b;
                 break;
         }
-        this.primaryDisplay.textContent = result;
+        this.primaryDisplay.textContent = this.formatter.format(result);
+        this.primaryDisplay.data.value = result;
         this.secondaryDisplay.textContent = '';
+        this.secondaryDisplay.data.value = '';
         this.operationDisplay.textContent = '';
         this.reset = true;
 
